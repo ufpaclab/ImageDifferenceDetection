b5c74328561fae126649de24d76ba64c39d275b1
Grey Johnson <grey.johnson@ufl.edu>
2021-03-07 18:22:29 -0500

    Updated with questions

--- ImageDifferenceDetection.js
+++ ImageDifferenceDetection.js
@@ -5,6 +5,34 @@ function ImageDifferenceDetection(jsSheetHandle, jsPsychHandle, survey_code) {
         // Define Constants
         const CONTACT_EMAIL = 'fake@email.com'
 
+        const IMAGE_QUESTIONS = [
+            {
+                question: 'How much does the meaning change from one picture to the other?',
+                leftLabel: 'Insignificant Change',
+                rightLabel: 'Very Significant Change'
+            },
+            {
+                question: 'How weird is the image?',
+                leftLabel: 'Very Normal',
+                rightLabel: 'Very Weird'
+            },
+            {
+                question: 'How likely is it to see this image in the real world?',
+                leftLabel: 'Very Unlikely',
+                rightLabel: 'Very Likely'
+            },
+            {
+                question: 'How hard is it to identify the object?',
+                leftLabel: 'Very Easy',
+                rightLabel: 'Very Hard'
+            },
+            {
+                question: 'How visually complicated is the image?',
+                leftLabel: 'Very Simple',
+                rightLabel: 'Very Complicated'
+            }
+        ]
+
         // Define Experiment Trials
         let welcomeTrial = {
             type: 'html-keyboard-response',
@@ -29,24 +57,29 @@ function ImageDifferenceDetection(jsSheetHandle, jsPsychHandle, survey_code) {
                             <img class="differenceDetectionElement differenceDetectionImage" src="resources/${jsPsychHandle.timelineVariable('rightImage', true)}"/>
                         </div>
                         <br></br>
-                        <p>How much does the meaning change from one picture to the other?</p>
+                        <p>${jsPsychHandle.timelineVariable('question', true)}</p>
                         <div style="overflow: hidden">
-                            <p style="float: left">Not at all</p>
-                            <p style="float: right">Totally</p>
+                            <p style="float: left">${jsPsychHandle.timelineVariable('leftLabel', true)}</p>
+                            <p style="float: right">${jsPsychHandle.timelineVariable('rightLabel', true)}</p>
                         </div>
                         `
                     },
                 }
             ],
             timeline_variables: function() {
-                let images = []
-                for (let i = 0; i < IMAGE_MANIFEST.length; i++) {
-                    images.push({
-                        leftImage: `${IMAGE_MANIFEST[i].name}.${IMAGE_MANIFEST[i].extension}`,
-                        rightImage: `${IMAGE_MANIFEST[i].name}_2.${IMAGE_MANIFEST[i].extension}`
-                    })
+                let variables = []
+                for (let image = 0; image < IMAGE_MANIFEST.length; image++) {
+                    for (let question = 0; question < IMAGE_QUESTIONS.length; question++) {
+                        variables.push({
+                            leftImage: `${IMAGE_MANIFEST[image].name}.${IMAGE_MANIFEST[image].extension}`,
+                            rightImage: `${IMAGE_MANIFEST[image].name}_2.${IMAGE_MANIFEST[image].extension}`,
+                            question: IMAGE_QUESTIONS[question].question,
+                            leftLabel: IMAGE_QUESTIONS[question].leftLabel,
+                            rightLabel: IMAGE_QUESTIONS[question].rightLabel
+                        })
+                    }
                 }
-                return images;
+                return variables;
             }()
         }
 
