e939a51094699a8d1ed9af672bf418933792c265
Grey Johnson <grey.johnson@ufl.edu>
2021-05-06 15:57:09 -0400

    manifest generator file

---
+++ scripts/generateManifest.py
@@ -0,0 +1,24 @@
+import os
+import re
+
+class Manifest():
+  def __init__(self):
+    self.data = "const IMAGE_MANIFEST = ["
+
+  def addFile(self, filename):
+    self.data = self.data + f"{{name: '{fileName}',extension: 'jpg',}},"
+
+  def toString(self):
+    return self.data + "];"
+
+manifest = Manifest()
+
+fileNames = os.listdir()
+for fileName in fileNames:
+  isProperFileTypeAndName = re.search(".*_2\\.jpg", fileName) != None
+  if (isProperFileTypeAndName):
+    fileName = fileName.replace("_2.jpg", "")
+    manifest.addFile(fileName)
+
+fileHandle = open("ImageManifest.js", "w")
+fileHandle.write(manifest.toString())
